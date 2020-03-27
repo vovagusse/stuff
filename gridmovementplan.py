f90dadebbc440f98052ac61726b8f51ddd2ba97d
vovagusse <61916942+vovagusse@users.noreply.github.com>
2020-03-27 17:56:46 +0300

    So thats my plan for grid movrmrnt thingy
    imma get coffee mate ugh

---
+++ gridmovementplan.py
@@ -0,0 +1,50 @@
+print('''
+
+1)Tuples with "_" as empty space and a * as a player
+2)Def's. A lot of them. to go right, left, up, down
+3)Input number "n" is making the grid "n" by "n" steps, each step represents a part of a tuple. 
+So to make it i should set the 1st tuple's 0th digit equal to "*" and the 1 to n-1 digit equal to "_" 
+4) Typing whether to go any direction should be "goto_dir = goto_dir.lower()" or smth.... i dunno
+5) Going outside of the grid should be like in pacman lol
+6) After each movement the algorithm should rerender the grid with the player
+moved the typed direction... also, "end" execution should turn an iterable "game" from
+"game = True"
+to
+"game = False"
+
+while game = True:
+    {blah blah blah}
+else print("okay bro thanks for using my little program")
+
+like that:
+
+Grid's side (its a square): 5
+Here's the grid you've made!
+* _ _ _ _
+_ _ _ _ _ 
+_ _ _ _ _
+_ _ _ _ _ 
+_ _ _ _ _
+Where to go? right
+_ * _ _ _
+_ _ _ _ _ 
+_ _ _ _ _
+_ _ _ _ _ 
+_ _ _ _ _
+Where to go? up
+_ _ _ _ _
+_ _ _ _ _ 
+_ _ _ _ _
+_ _ _ _ _ 
+_ * _ _ _
+Where to go? end
+okay bro thanks for using my little program
+
+
+^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
+| | | | | | | | | | | | | | | | | | | | | | | | 
+thats what the program shoud look like in theory
+aaaaaand im going to finish it :)))))))))
+
+aight imma get some coffee
+''')
